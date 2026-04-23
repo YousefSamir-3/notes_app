a97098ef8cf06f68b3f6761f5aa1fc83f1d80796
Yousef Samir <yousefsamir5321@gmail.com>
2026-04-23 10:07:24 +0200

    show model bottom sheet and Edit floting

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,7 +9,17 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddNoteBottomSheet();
+            },
+          );
+        },
+        backgroundColor: Colors.lightBlueAccent, // لبني (أزرق فاتح)
+        foregroundColor: Colors.black, // لون الأيقونة
+        shape: const CircleBorder(), // يخليه دايرة صريحة
         child: const Icon(Icons.add),
       ),
       body: const HomeViewBody(),
